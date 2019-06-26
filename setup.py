d19bcd9a24a504c72ef582e855ca8c1812bbee98
LachlanMcLachlan <lachlan.mclachlan@outlook.com>
2019-06-26 20:10:03 +0100

    initialise git repo in package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tdd_tutorial',
+      version='1.0',
+      description='package containing code for the python tdd tutorial',
+      author='Lachlan McLachlan',
+      author_email='lachlan.mclachlan@outlook.com',
+      license='MIT',
+      packages=['tdd_tutorial'],
+      zip_safe=False)
